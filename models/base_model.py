55f1cf541c28a9275bc45e24dfbc73af39bbb921
abdelghaniabb <abdelghani.aitbenbraim@gmail.com>
2023-12-06 02:06:35 +0100

    unittest

--- models/base_model.py
+++ models/base_model.py
@@ -5,13 +5,14 @@
 import uuid
 import datetime
 
+
 class BaseModel():
-        """
+    """
         defines all common attributes/methods for other classes
-        """
+    """
     def __init__(self):
         """
-        Initializes a new instance of BaseModel.
+            Initializes a new instance of BaseModel.
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
@@ -19,21 +20,22 @@ class BaseModel():
 
     def __str__(self):
         """
-        Returns a string representation of BaseModel instance.
+            Returns a string representation of BaseModel instance.
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-    
+        c_n = self.__class__.__name__
+        return "[{}] ({}) {}".format(c_n, self.id, self.__dict__)
+
     def save(self):
         """
-        Updates the public instance attribute
-        updated_at with the current datetime
+            Updates the public instance attribute
+            updated_at with the current datetime
         """
         self.updated_at = datetime.datetime.now()
 
     def to_dict(self):
-        """ 
-        Returns a dictionary containing
-        all keys/values of __dict__ of the instance
+        """
+            Returns a dictionary containing
+            all keys/values of __dict__ of the instance
         """
         my_dict = self.__dict__
         my_dict['__dict__'] = self.__class__.__name__
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,14 +1,41 @@
 #!/usr/bin/python3
-from models.base_model import BaseModel
+"""test_base_model"""
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+from models.base_model import BaseModel
+from datetime import datetime
+import unittest
+
+
+class TestBaseModel(unittest.TestCase):
+    """test BaseModel class"""
+
+    def test_class_type(self):
+        """test the type of the instance atributes"""
+        my_model = BaseModel()
+        self.assertIsInstance(my_model, BaseModel)
+        self.assertIsInstance(my_model.created_at, datetime)
+        self.assertIsInstance(my_model.updated_at, datetime)
+
+    def test_str_(self):
+        """test the __str__ method"""
+        my_model = BaseModel()
+        str_ = my_model.__str__()
+        id = my_model.id
+        dic = my_model.__dict__
+        self.assertEqual(str_, "[BaseModel] ({}) {}".format(id, dic))
+
+    def test_save(self):
+        """test the save method"""
+        my_model = BaseModel()
+        created_at = my_model.created_at
+        my_model.save()
+        self.assertIs(my_model.created_at, created_at)
+        self.assertIsNot(my_model.updated_at, my_model.created_at)
+
+    def test_to_dict(self):
+        """test the to_dict method"""
+        my_model = BaseModel()
+        my_model_json = my_model.to_dict()
+        self.assertNotIsInstance(my_model.__str__(), dict)
+        self.assertIsInstance(my_model_json, dict)
